0920d1e102856315432db741dd02a96f1c5ec44c
Christian Simonsen <crsdev@gmail.com>
2025-06-26 10:35:09 +0200

    Updated build scripts, added suport for bootstrap. Parcel build now part of github actions

--- src/app.mjs
+++ src/app.mjs
@@ -1,3 +1,4 @@
+import * as bootstrap from "bootstrap"
 import { readBarcodes, writeBarcode } from "zxing-wasm";
 
 const options = { format: "QRCode", scale: 15, EcLevel: "L" };
@@ -8,15 +9,20 @@ const output = document.getElementById("output");
 
 let qrCode = null;
 
+togleVisibility(downloadBT, output);
 
 generateBt.onclick = async (e) => {
-    qrCode = await writeBarcode(input.value, options)
+
+    if (!qrCode) {
+        togleVisibility(downloadBT, output);
+    }
+
+    qrCode = await writeBarcode(input.value, options);
     output.setAttribute("src", URL.createObjectURL(qrCode.image));
     downloadBT.removeAttribute("disabled")
 }
 
 downloadBT.onclick = async (e) => {
-    console.log("wtf");
     let a = document.createElement("a");
     a.href = "data:image/png;base64," + await blobToBase64(qrCode.image);
     a.download = "qr.png";
@@ -26,15 +32,22 @@ downloadBT.onclick = async (e) => {
 function blobToBase64(blob) {
     return new Promise((resolve, reject) => {
         const reader = new FileReader();
-
         reader.onloadend = () => {
             const base64String = reader.result.split(',')[1];
             resolve(base64String);
         };
 
         reader.onerror = reject;
-
         reader.readAsDataURL(blob);
     });
 }
 
+function togleVisibility(...items) {
+    for (let item of items) {
+        if (item.style.visibility === "hidden") {
+            item.style.visibility = "visible";
+        } else {
+            item.style.visibility = "hidden";
+        }
+    }
+}
